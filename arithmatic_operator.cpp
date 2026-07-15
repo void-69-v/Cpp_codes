314647917d2bc6f4266ccb321ead8d7126e834d3
Void <k.void.108@gmail.com>
2026-07-15 23:02:51 +0530

    Simplify arithmetic operations and output result
    Removed commented-out code and added division output.

--- arithmatic_operator.cpp
+++ arithmatic_operator.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float x=7; float  y=3;
+ /*
+    cout <<"Enter the value : "; 
+    cin >> x;
+    cout <<"The value is - "<<x;
+    cout <<x<<endl;
+      
+    x = x-10;
+    cout << x<<'\n';
+    x = x+2;
+    x/=y;*/
+    //float x = 10.5;
+    cout <<x/y<<endl;
 
+}
